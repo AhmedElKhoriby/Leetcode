dd4e2af9854ca9a82692a19bab804dc799c04406
Ahmed El-Khoriby <137055432+AhmedElKhoriby@users.noreply.github.com>
2024-06-07 01:06:45 +0300

    Time: 0 ms (100.00%) | Memory: 10.5 MB (6.14%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -1,17 +1,10 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        sort(nums.begin(), nums.end());
-        int p1 = 0, p2 = nums.size()-1, ctr = 0;
-        while (p1 <= p2) {
-            int &val1 = nums[p1], &val2 = nums[p2];
-            if (val1 == val) {
-                swap(nums[p1],  nums[p2]);
-                ctr++;
-                p2--;
-            }
-            else p1++;
-        }
-        return nums.size() - ctr;
+        int k = 0,sz = nums.size();
+        for (int i = 0; i < sz;i++)
+            if (nums[i] != val)
+                nums[k++] = nums[i];
+        return k;
     }
 };

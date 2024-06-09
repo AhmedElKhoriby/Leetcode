e8cfa0387c04de308f65d47c203ca30130405829
Ahmed El-Khoriby <137055432+AhmedElKhoriby@users.noreply.github.com>
2024-06-09 04:47:53 +0300

    Time: 0 ms (100.00%) | Memory: 7.7 MB (45.61%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        int i = 0, sz1 = word1.size(), sz2 = word2.size();
-        string ans = "";
-        while (i < sz1 && i < sz2) {
-            ans = ans + word1[i] + word2[i++];
+        string res = "";
+        int i = 0;
+        for (; i < word1.size() && i < word2.size(); i++) {
+            res += word1[i];
+            res += word2[i];
         }
-        while (i < sz1) {
-            ans += word1[i++];
-        }
-        while (i < sz2) {
-            ans += word2[i++];
-        }
-        return ans;
+        if (i < word1.size()) res.append(word1.substr(i));
+        if (i < word2.size()) res.append(word2.substr(i));
+        return res;
     }
 };

d6525e3425454dc12cb4d1bf74f1edceecb3c7be
Ahmed El-Khoriby <137055432+AhmedElKhoriby@users.noreply.github.com>
2024-07-01 15:31:20 +0300

    Time: 7 ms (51.55%) | Memory: 19.9 MB (44.87%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.php
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.php
@@ -1,12 +1,31 @@
-class Solution {
+
+function srtposition($haystack, $needle)
+{
+    $sz1 = strlen($haystack);
+    $sz2 = strlen($needle);
+    for ($i = 0; $i <= $sz1 - $sz2; $i++) {
+        $match = true;
+        for($j = 0;$j < $sz2;$j++){
+            if($haystack[$j + $i]!==$needle[$j]){
+                $match = false;
+                break;
+            }
+        }
+        if($match) return $i;
+    }
+    return false;
+}
+class Solution
+{
 
     /**
      * @param String $haystack
      * @param String $needle
      * @return Integer
      */
-    function strStr($haystack, $needle) {
-        $position = strpos($haystack,$needle);
+    function strStr($haystack, $needle)
+    {
+        $position = srtposition($haystack, $needle);
         return (($position === false) ? -1 : $position);
     }
 }

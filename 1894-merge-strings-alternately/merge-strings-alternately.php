a1ff2a0228b9033263e6b8f462919050ed06d46d
Ahmed El-Khoriby <137055432+AhmedElKhoriby@users.noreply.github.com>
2024-07-01 13:04:30 +0300

    Time: 6 ms (62.05%) | Memory: 19.8 MB (72.05%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.php
@@ -0,0 +1,24 @@
+class Solution {
+
+    /**
+     * @param String $word1
+     * @param String $word2
+     * @return String
+     */
+    function mergeAlternately($word1, $word2) {
+        $answer = ""; $i = 0 ; $j = 0;
+        $sz1 = mb_strlen($word1, 'UTF-8');
+        $sz2 = mb_strlen($word2, 'UTF-8');
+        while ($i < $sz1 && $j < $sz2){
+            $answer .= $word1[$i++];
+            $answer .= $word2[$j++];
+        }
+        while ($i < $sz1){
+            $answer .= $word1[$i++];
+        }
+        while ($j < $sz2){
+            $answer .= $word2[$j++];
+        }
+        return $answer;
+    }
+}

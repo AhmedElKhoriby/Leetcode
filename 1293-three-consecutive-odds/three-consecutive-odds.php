db06c9777453892b7cc782a67888685e2dbf6ea3
Ahmed El-Khoriby <137055432+AhmedElKhoriby@users.noreply.github.com>
2024-07-01 12:53:29 +0300

    Time: 14 ms (20.00%) | Memory: 20.1 MB (60.00%) - LeetSync

---
+++ 1293-three-consecutive-odds/three-consecutive-odds.php
@@ -0,0 +1,19 @@
+class Solution {
+
+    /**
+     * @param Integer[] $arr
+     * @return Boolean
+     */
+    function threeConsecutiveOdds($arr) {
+        $count = 0;
+        foreach ($arr as $num){
+            if($num % 2 != 0){
+                $count++;
+                if($count == 3) return true;
+            }else{
+                $count = 0;
+            }
+        }
+        return 0;
+    }
+}

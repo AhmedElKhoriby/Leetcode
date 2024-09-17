579861bc83e72e428e7d78791ddbd126f83cb746
Ahmed El-Khoriby <137055432+AhmedElKhoriby@users.noreply.github.com>
2024-09-17 17:27:49 +0300

    Time: 19 ms (48.23%) | Memory: 28.1 MB (15.25%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,8 +1,10 @@
+#include<bits/stdc++.h>
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         int main=nums.size();
          return nums[main/2];
-    } 
+    }
+   
 };

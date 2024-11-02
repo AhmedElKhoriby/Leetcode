d20f112f7f1ee6ce436da71946fd3167018eddf3
Ahmed El-Khoriby <137055432+AhmedElKhoriby@users.noreply.github.com>
2024-11-02 08:54:35 +0200

    Time: 0 ms (100.00%) | Memory: 8.2 MB (59.78%) - LeetSync

---
+++ 2580-circular-sentence/circular-sentence.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isCircularSentence(string sentence) {
+        int sz = sentence.size();
+        if(sentence[0]!=sentence[sz-1]) return 0;
+        for (int i = 1; i < sz-1; i++) {
+            if(sentence[i]==' ' && i > 0 && i < sz - 1 && sentence[i-1]!= sentence[i+1]){
+                return 0;
+            }
+        }
+        return 1;
+    }
+};
